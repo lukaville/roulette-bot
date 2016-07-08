8db565c5d2a5c40ced355c239f67e165e408a9c0
Nickolay <lukaville@gmail.com>
2016-07-08 17:49:22 +0300

    Update help

--- i18n.py
+++ i18n.py
@@ -1,7 +1,7 @@
 # TODO: extract to files, add multi-language support, use built-in i18n module\
 
 DEFAULT_LOCALE = {
-    'HELP': "Use /roulette command to connect with random user",
+    'HELP': "Use /roulette command to connect with random user,/n /disconnect to stop chatting",
     'START': "Hi! I can connect you with random stranger.\nLet's start! Use /roulette command.",
     'DISCONNECTED_SEARCH_NEW': "Disconnected. Searching new user...",
     'DISCONNECTED': "Disconnected. Use /roulette to connect to new user.",
